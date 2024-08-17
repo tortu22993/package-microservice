be621c1b17fa33196b8226b79420b6e20990777f
tortu22993 <enrique.escalante@hotmail.es>
2024-08-17 16:45:32 +0200

    entities

--- actualizacionEstados/src/main/java/com/enrique/actualizacionEstados/entity/Pack.java
+++ actualizacionEstados/src/main/java/com/enrique/actualizacionEstados/entity/Pack.java
@@ -1,14 +1,30 @@
 package com.enrique.actualizacionEstados.entity;
 
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Table(name = "Package")
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Pack {
 
+    @Id
+    @Column(name="id", updatable = false)
     private String id;
+
+    @Column(name = "nombre", nullable = false)
     private String name;
+
+    @Column(name = "status", nullable = false)
     private String status;
 }
--- gestionPaquetes/src/main/java/com/enrique/gestionPaquetes/entity/Pack.java
+++ gestionPaquetes/src/main/java/com/enrique/gestionPaquetes/entity/Pack.java
@@ -1,13 +1,29 @@
 package com.enrique.gestionPaquetes.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Table(name = "Package")
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Pack {
 
+    @Id
+    @Column(name="id", updatable = false)
     private String id;
+
+    @Column(name = "nombre", nullable = false)
     private String name;
+
+    @Column(name = "status", nullable = false)
     private String status;
 }
